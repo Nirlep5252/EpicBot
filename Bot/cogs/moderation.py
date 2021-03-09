07b1db12d2b34bcb965f5c9a7c1380599c9fa344
Nirlep_5252_ <70529587+Nirlep5252@users.noreply.github.com>
2021-03-09 17:07:38 +0530

    bad. ugly.
    e

--- Bot/cogs/moderation.py
+++ Bot/cogs/moderation.py
@@ -87,29 +87,7 @@ class Moderation(commands.Cog):
         await member.remove_roles(role)
         embed = discord.Embed(title = "Role Removed", description = f"Successfully removed {role.mention} role from {member.mention}.", color = 0xFF0000)
         await ctx.send(embed = embed)
-
-    # @commands.command()
-    # @commands.has_permissions(kick_members = True)
-    # async def warn(self, ctx, member: discord.Member, *, reason: str = None):
-    #
-    #     if int(ctx.author.top_role.position) < int(member.top_role.position):
-    #         await ctx.send(f"The user you are trying to warn has a higher role than you, so you can't warn them.")
-    #         return
-    #
-    #     if int(ctx.author.top_role.position) == int(member.top_role.position):
-    #         await ctx.send(f"The user you are trying to warn has the same role as you, so you can't warn them.")
-    #         return
-    #
-    #     if reason == None:
-    #         await member.create_dm()
-    #         await member.dm_channel.send(f":warning: **WARNING** :warning: \n\n-You have been warned by a moderator on the server `{ctx.guild}`\n\nReason: `No reason provided.`\nModerator: `{ctx.author.name}#{ctx.author.discriminator}`")
-    #         await ctx.send(f":warning: {member.mention} was warned.")
-    #         return
-    #
-    #     await member.create_dm()
-    #     await member.dm_channel.send(f":warning: **WARNING** :warning: \n\n-You have been warned by a moderator on the server `{ctx.guild}`\n\nReason: `{reason}`\nModerator: `{ctx.author.name}#{ctx.author.discriminator}`")
-    #     await ctx.send(f":warning: {member.mention} was warned.")
-
+        
     @commands.command(aliases = ['deletechannel', 'delete-channel', 'nukechannel'])
     @commands.has_permissions(manage_channels = True)
     async def delete_channel(self, ctx, channel: discord.TextChannel = None):
@@ -163,11 +141,6 @@ class Moderation(commands.Cog):
     async def delete_channel_error(self, ctx, error):
         if isinstance(error, commands.CommandInvokeError):
             await ctx.send(f"I don't think i have enough permissions to execute this task.")
-
-    # @warn.error
-    # async def warn_error(self, ctx, error):
-    #     if isinstance(error, commands.CommandInvokeError):
-    #         await ctx.send(f"An error occured. The user you are trying to warn either has their DMs closed or they have blocked me. Please try again later.")
 
     @ban.error
     async def ban_error(self, ctx, error):
