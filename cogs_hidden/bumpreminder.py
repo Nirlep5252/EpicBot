9306048922acd3ed62476a48f48fdecfc47f9a8a
nirlep5252 <hellonirlep@gmail.com>
2021-08-14 21:29:39 +0530

    mother fucker

--- cogs_hidden/bumpreminder.py
+++ cogs_hidden/bumpreminder.py
@@ -75,7 +75,7 @@ class BumpReminder(commands.Cog):
 
         await lemao_bumper.add_roles(role)
         await message.channel.send(
-            f"{bumper.mention} You have been rewarded the {role.mention} role for **2 hours**.",
+            f"{lemao_bumper.mention} You have been rewarded the {role.mention} role for **2 hours**.",
             delete_after=5
         )
 
