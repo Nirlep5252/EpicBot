530c28ca857cbc70380c5103defe8317139f0acb
LucasGamerz23 <75535434+LucasGamerz23@users.noreply.github.com>
2021-08-27 13:18:10 +0800

    PLS MERGE

--- cogs/fun.py
+++ cogs/fun.py
@@ -216,10 +216,16 @@ Another Example: `{prefix}shouldi Study OR Procrastinate`
     async def brainsize(self, ctx, user: Union[discord.Member, str] = None):
         user = user or ctx.author
         iq = random.randint(0, 200)
+        if ctx.author.id == 558861606063308822:
+            iq = 10000000000
         if iq == 0:
             color = RED_COLOR
             footer = "ur brain is just like my life. non existent."
             size = "no_brain"
+        elif iq == 10000000000:
+            color = MAIN_COLOR
+            footer = "Ah yes the best genius guy in the world THE GENIUSLEP!"
+            size = 'big'
         elif iq in range(1, 50):
             color = RED_COLOR
             footer = "lol ur brain is almost the same size as ur pp"
