cec90744beec10d891175a6ef5e35c6576188213
nirlep5252 <hellonirlep@gmail.com>
2021-10-07 19:06:28 +0530

    ok whatever im desperate

--- utils/bot.py
+++ utils/bot.py
@@ -49,7 +49,7 @@ class EpicBot(commands.AutoShardedBot):
             help_command=EpicBotHelp(),
             cached_messages=10000,
             activity=discord.Activity(type=discord.ActivityType.playing, name="e!help | epic-bot.com" if not beta else "nirlep is doing some weird shit rn"),
-            shard_count=2 if not self.beta else 1  # remove this if your bot is under 1000 servers
+            shard_count=4 if not self.beta else 1  # remove this if your bot is under 1000 servers
         )
         cluster = motor.AsyncIOMotorClient(MONGO_DB_URL if not beta else MONGO_DB_URL_BETA)
         self.session = aiohttp.ClientSession()
