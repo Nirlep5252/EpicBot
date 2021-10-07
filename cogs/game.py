92b2f057da6f8d4f4b2816fb747120e1b854d9d3
nirlep5252 <hellonirlep@gmail.com>
2021-10-07 19:13:07 +0530

    flake8 and stuff

--- cogs/game.py
+++ cogs/game.py
@@ -1,9 +1,12 @@
 """
 Copyright 2021 Nirlep_5252_
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,15 +16,14 @@ limitations under the License.
 
 import discord.ui
 import asyncio
-from config import MAIN_COLOR, EMPTY_CHARACTER
+from config import MAIN_COLOR
 import random
-from utils.embed import success_embed, error_embed
 from discord.ext import commands
 from utils.message import wait_for_msg
 from games import tictactoe, wumpus, minesweeper, twenty
 from utils.bot import EpicBot
-from utils.message import wait_for_msg
 from Discord_Games import aki_buttons
+
 
 class TruthAndDareView(discord.ui.View):
     def __init__(self, ctx: commands.Context):
@@ -109,11 +111,6 @@ class games(commands.Cog, description="Play some fun games with me!"):
             else:
                 await ctx.send("**I win :robot:**")
 
-# this isn't the perfect edition
-# I wanted to make a system in which the bot check if the message contains any attachments
-# then it will send the final message of the dare part
-# if ur good in Koding then make a good one urself
-
     @commands.command(aliases=['tnd'], help="Play truth and dare!")
     @commands.cooldown(1, 30, commands.BucketType.user)
     @commands.max_concurrency(1, commands.BucketType.user)
@@ -125,41 +122,42 @@ class games(commands.Cog, description="Play some fun games with me!"):
             return await main_msg.edit(content="Command cancelled or timed out!", view=None)
         if view.value == 'truth':
             truth_is_always_painful = random.choice([
-                 "When was the last time you lied?",
-                 "When was the last time you cried?",
-                 "What's your biggest fear?",
-                 "What's your biggest fantasy?",
-                 "Do you have any fetishes?",
-                 "What's something you're glad your mum doesn't know about you?",
-                 "Have you ever cheated on someone?",
-                 "What's the worst thing you've ever done?",
-                 "What's a secret you've never told anyone?",
-                 "Do you have a hidden talent?",
-                 "Who was your first celebrity crush?",
-                 "What are your thoughts on polyamory?",
-                 "What's the worst intimate experience you've ever had?",
-                 "Have you ever cheated in an exam?",
-                 "What's the most drunk you've ever been?",
-                 "Have you ever broken the law?",
-                 "What's the most embarrassing thing you've ever done?",
-                 "What's your biggest insecurity?",
-                 "What's the biggest mistake you've ever made?",
-                 "What's the most disgusting thing you've ever done?",
-                 "Who would you like to kiss in this room?",
-                 "What's the worst thing anyone's ever done to you?",
-                 "Have you ever had a run in with the law?",
-                 "What's your worst habit?",
-                 "What's the worst thing you've ever said to anyone?",
-                 "Have you ever peed in the shower?",
-                 "What's the strangest dream you've had?",
-                 "Have you ever been caught doing something you shouldn't have?",
-                 "What's the worst date you've been on?",
-                 "What's your biggest regret?",
-                 "What's the biggest misconception about you?",
-                 "Where's the weirdest place you've had sex?",
-                 "Why did your last relationship break down?",
-                 "Have you ever lied to get out of a bad date?",
-                 "What's the most trouble you've been in?"])
+                "When was the last time you lied?",
+                "When was the last time you cried?",
+                "What's your biggest fear?",
+                "What's your biggest fantasy?",
+                "Do you have any fetishes?",
+                "What's something you're glad your mum doesn't know about you?",
+                "Have you ever cheated on someone?",
+                "What's the worst thing you've ever done?",
+                "What's a secret you've never told anyone?",
+                "Do you have a hidden talent?",
+                "Who was your first celebrity crush?",
+                "What are your thoughts on polyamory?",
+                "What's the worst intimate experience you've ever had?",
+                "Have you ever cheated in an exam?",
+                "What's the most drunk you've ever been?",
+                "Have you ever broken the law?",
+                "What's the most embarrassing thing you've ever done?",
+                "What's your biggest insecurity?",
+                "What's the biggest mistake you've ever made?",
+                "What's the most disgusting thing you've ever done?",
+                "Who would you like to kiss in this room?",
+                "What's the worst thing anyone's ever done to you?",
+                "Have you ever had a run in with the law?",
+                "What's your worst habit?",
+                "What's the worst thing you've ever said to anyone?",
+                "Have you ever peed in the shower?",
+                "What's the strangest dream you've had?",
+                "Have you ever been caught doing something you shouldn't have?",
+                "What's the worst date you've been on?",
+                "What's your biggest regret?",
+                "What's the biggest misconception about you?",
+                "Where's the weirdest place you've had sex?",
+                "Why did your last relationship break down?",
+                "Have you ever lied to get out of a bad date?",
+                "What's the most trouble you've been in?"
+            ])
             await main_msg.edit(content=f"```\n{truth_is_always_painful}\n```", view=None)
             msg_check = await wait_for_msg(ctx, 60, main_msg)
             if msg_check == 'pain':
@@ -168,47 +166,49 @@ class games(commands.Cog, description="Play some fun games with me!"):
                 await main_msg.edit(content="Oh, is that so? 😏 I didn't knew that. **Shame! Shame!**", view=None)
         elif view.value == 'dare':
             dare_is_more_and_always_painful = random.choice([
-                 "Show the most embarrassing photo on your phone",
-                 "Show the last five people you texted and what the messages said",
-                 "Let the rest of the group DM someone from your Instagram account",
-                 "Eat a raw piece of garlic",
-                 "Do 100 squats",
-                 "Keep three ice cubes in your mouth until they melt",
-                 "Say something dirty to the person on your left",
-                 "Give a foot massage to the person on your right",
-                 "Put 10 different available liquids into a cup and drink it",
-                 "Yell out the first word that comes to your mind",
-                 "Give a lap dance to someone of your choice",
-                 "Remove four items of clothing",
-                 "Like the first 15 posts on your Facebook newsfeed",
-                 "Eat a spoonful of mustard",
-                 "Keep your eyes closed until it's your go again",
-                 "Send a sext to the last person in your phonebook",
-                 "Show off your orgasm face",
-                 "Seductively eat a banana",
-                 "Empty out your wallet/purse and show everyone what's inside",
-                 "Do your best sexy crawl",
-                 "Pretend to be the person to your right for 10 minutes",
-                 "Eat a snack without using your hands",
-                 "Say two honest things about everyone else in the group",
-                 "Twerk for a minute",
-                 "Try and make the group laugh as quickly as possible",
-                 "Try to put your whole fist in your mouth",
-                 "Tell everyone an embarrassing story about yourself",
-                 "Try to lick your elbow",
-                 "Post the oldest selfie on your phone on Instagram Stories",
-                 "Tell the saddest story you know",
-                 "Howl like a wolf for two minutes",
-                 "Dance without music for two minutes",
-                 "Pole dance with an imaginary pole",
-                 "Let someone else tickle you and try not to laugh",
-                 "Put as many snacks into your mouth at once as you can"])
+                "Show the most embarrassing photo on your phone",
+                "Show the last five people you texted and what the messages said",
+                "Let the rest of the group DM someone from your Instagram account",
+                "Eat a raw piece of garlic",
+                "Do 100 squats",
+                "Keep three ice cubes in your mouth until they melt",
+                "Say something dirty to the person on your left",
+                "Give a foot massage to the person on your right",
+                "Put 10 different available liquids into a cup and drink it",
+                "Yell out the first word that comes to your mind",
+                "Give a lap dance to someone of your choice",
+                "Remove four items of clothing",
+                "Like the first 15 posts on your Facebook newsfeed",
+                "Eat a spoonful of mustard",
+                "Keep your eyes closed until it's your go again",
+                "Send a sext to the last person in your phonebook",
+                "Show off your orgasm face",
+                "Seductively eat a banana",
+                "Empty out your wallet/purse and show everyone what's inside",
+                "Do your best sexy crawl",
+                "Pretend to be the person to your right for 10 minutes",
+                "Eat a snack without using your hands",
+                "Say two honest things about everyone else in the group",
+                "Twerk for a minute",
+                "Try and make the group laugh as quickly as possible",
+                "Try to put your whole fist in your mouth",
+                "Tell everyone an embarrassing story about yourself",
+                "Try to lick your elbow",
+                "Post the oldest selfie on your phone on Instagram Stories",
+                "Tell the saddest story you know",
+                "Howl like a wolf for two minutes",
+                "Dance without music for two minutes",
+                "Pole dance with an imaginary pole",
+                "Let someone else tickle you and try not to laugh",
+                "Put as many snacks into your mouth at once as you can"
+            ])
             await main_msg.edit(content=f"```\n{dare_is_more_and_always_painful}\n```", view=None)
             msg_check = await wait_for_msg(ctx, 60, main_msg)
             if msg_check == 'pain':
                 return
             else:
                 await main_msg.edit(content="Oh, seems like u have some guts. Well done.", view=None)
+
 
 def setup(client):
     client.add_cog(games(client))
